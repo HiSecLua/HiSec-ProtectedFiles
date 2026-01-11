bcf1ec09808e44f61dba2bd22376a55a4ea60e31
JaiTapper <delunacarl7@gmail.com>
2026-01-12 06:18:27 +0800

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "280028", AccentColor = "7d007d", BackgroundColor = "220022", OutlineColor = "1b1b1b" },
+            { FontColor = "ffffff", MainColor = "280028", AccentColor = "7d007d", BackgroundColor = "220022", OutlineColor = "48004e" },
         },
         ["BBot"] = {
             2,
